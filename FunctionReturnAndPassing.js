44996378720569c4a86dc79690e201703af997ea
Bluefalcon04v <101953634+Bluefalcon04v@users.noreply.github.com>
2022-07-05 00:41:09 +0530

    Add files via upload
    9th Function Return And Passing value

---
+++ FunctionReturnAndPassing.js
@@ -0,0 +1,11 @@
+// Functions in that we Return value and Pass value
+
+let add = (num1,num2) => {           // Here we are passing the values of num1 and num2
+    return num1 + num2               // Here we are returning it
+}
+
+let print = add(4 , 5);              // to print return statements we have to assign it to new variable 
+console.log(print);
+
+
+// Note When a return statement is used in a function body, The excution of the function stops
