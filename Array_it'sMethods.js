090097581b10b00aa9ac2ae6a34e438f04a81bc8
Bluefalcon04v <101953634+Bluefalcon04v@users.noreply.github.com>
2022-07-07 18:06:04 +0530

    Add files via upload
    14th About Arrays and it's Methods

---
+++ Array_it'sMethods.js
@@ -0,0 +1,21 @@
+// Creating Arrays and Fetching data from it, Arrays Methods
+
+let values = [2, , 4, 'Hello', {tech:'javaScript'}, function(){console.log("i love JS");}];
+console.log(values);
+console.log(values[4]);      
+values[5]();             // calling function in array
+
+
+// Array Methods
+console.log("Arrays Methods");
+values.pop();              // to remove last element from array
+console.log(values);
+values.shift();            // to remove element at the start
+console.log(values);
+values.push(9, 6, 4)          // to push element in array at last place
+console.log(values);
+values.unshift(3, 10)         // to push element in array at first place
+console.log(values);
+console.log(values.length);    // to check the length of the array
+values.splice(2, 3, 'Hey', 'Bye');  // '2' indicates index value, '3' indicates number of values to remove, rest will be replacing the removed values
+console.log(values);
