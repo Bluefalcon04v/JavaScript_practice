32abd480449b0d2dc4d612e3dbddbdf6f3330c06
AmitSah <101953634+Bluefalcon04v@users.noreply.github.com>
2022-07-20 13:32:59 +0530

    Add files via upload
    adding Stop propagation in the code of Event Propagation

--- About DOM/Event Bubbling and Event Capturing/script.js
+++ About DOM/Event Bubbling and Event Capturing/script.js
@@ -7,30 +7,33 @@
 // true means Event Capturing)
 
 // Event Bubbling
-document.querySelector('#grandParent').addEventListener('click', () => {
-    console.log("Grand Parent is clicked");
-}, false)
+// document.querySelector('#grandParent').addEventListener('click', () => {
+//     console.log("Grand Parent is clicked");
+// }, false)
 
-document.querySelector('#parent').addEventListener('click', () => {
-    console.log("Parent is clicked");
-}, false)
+// document.querySelector('#parent').addEventListener('click', () => {
+//     console.log("Parent is clicked");
+// }, false)
 
-document.querySelector('#child').addEventListener('click', () => {
-    console.log("Child is clicked");
-}, false)
+// document.querySelector('#child').addEventListener('click', () => {
+//     console.log("Child is clicked");
+// }, false)
 
 // Event Capturing
 document.querySelector('#grandParent').addEventListener('click', () => {
     console.log("Grand Parent is clicked");
 }, true)
 
-document.querySelector('#parent').addEventListener('click', () => {
+document.querySelector('#parent').addEventListener('click', (e) => {
     console.log("Parent is clicked");
+    e.stopPropagation();
 }, true)
 
 document.querySelector('#child').addEventListener('click', () => {
     console.log("Child is clicked");
 }, true)
 
-
 // Note "if you want to notice difference just comment out one type before running it"
+
+// Now to stop the Propagation of Events We can use Inbuild method with the help of "Event Object 'e'" 
+// and in build function "e.stopPropagation()"
